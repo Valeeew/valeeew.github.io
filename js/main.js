b976bf4e980ef942ada09ca792d86e0acff5166c
Valentin MONTEIRO <73248254+Valeeew@users.noreply.github.com>
2022-01-08 12:12:37 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -14,9 +14,7 @@ jQuery(document).ready(function($) {
             pauseOnDotsHover:true,
             cssEase:'linear',
            // fade:true,
-            draggable:false,
-            prevArrow:'<button class="PrevArrow"></button>',
-            nextArrow:'<button class="NextArrow"></button>', 
+            draggable:false, 
           });
 
         $('#nav-toggle').on('click', function (event) {
